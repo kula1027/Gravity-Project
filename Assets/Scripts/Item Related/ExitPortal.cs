5c7be124a5d14374ed6140faa62013c8f453ec2f
kula1027 <jeff5289@naver.com>
2015-07-21 22:13:46 +0900

    Exit Portal 구현 Stage 0 조금 수정

---
+++ Assets/Scripts/Item Related/ExitPortal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitPortal : MonoBehaviour {
+	public string NextStage;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		if (coll.transform.name.Equals ("Player")) {
+			StartCoroutine("LoadSceneInSeconds", 3);
+		}
+	}
+
+	IEnumerator LoadSceneInSeconds(int secs){
+		yield return new WaitForSeconds(secs);
+
+		Application.LoadLevel (NextStage);
+	}
+}
